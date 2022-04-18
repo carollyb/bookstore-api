102120b0ee8476d2c389a365f05c26e7b83b0b42
carollyb <carolly.barbosa@gmail.com>
2022-04-18 16:21:10 -0300

    feat: list all users and list user by username - search users in database

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 const CreateUserController = require("../controllers/user/CreateUserController");
+const ListAllUsersController = require("../controllers/user/ListAllUsersController");
+const ListByUsernameUserController = require("../controllers/user/ListByUsernameUserController");
 
 const router = Router();
 
 router.post("/", CreateUserController.handle);
+
+router.get("/", ListAllUsersController.handle)
+router.get("/:username", ListByUsernameUserController.handle)
 
 module.exports = router
